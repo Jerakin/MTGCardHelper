970abe46e9625d5ce6a6e4325b747b97ee7028ea
Jerakin <the_rensvind@hotmail.com>
2022-06-19 16:55:37 +0200

    Changed name

--- jsx/PPRO/Premiere.jsx
+++ jsx/PPRO/Premiere.jsx
@@ -2,7 +2,7 @@
 
 $._PPP_={
 	keepPanelLoaded : function () {
-		app.setExtensionPersistent("com.adobe.PProPanel", 0); // 0, while testing (to enable rapid reload); 1 for "Never unload me, even when not visible."
+		app.setExtensionPersistent("com.jerakin.mtgcardhelper", 0); // 0, while testing (to enable rapid reload); 1 for "Never unload me, even when not visible."
 	},
 
 	searchForBinWithName : function (nameToFind) {
